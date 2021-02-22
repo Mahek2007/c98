ca98e933cf35f3882495df629f1ac4c384ad458d
Mahek2007 <60539335+Mahek2007@users.noreply.github.com>
2021-02-22 17:02:42 +0530

    Add files via upload

---
+++ algorithms1.py
@@ -0,0 +1,25 @@
+#number=int(input('enter the number you want to check'))
+"""if number %2==0:
+    print( 'your number is even')
+else:
+    print('your number is odd')"""
+
+"""for i in range(0,number):
+    if i %2==0:
+        print(i)"""
+
+
+
+
+"""for i in range(2,number):
+    if i>1:
+
+     for num in range(2,i):
+        if i %num==0:
+            break
+     else:
+        print(i)"""
+name=input('whats your name?')
+def greet(name):
+    print('hello '+name)
+greet(name)
